870d5ddaa71d302dde5bf1bca4f40ffbd16b6c0b
pxmayang <peyangpetrina18@gmail.com>
2022-01-25 14:41:08 +0400

    Added scrollable tabs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 //import './App.css';
 import VerticalTabs from './VerticalTabs.js'
+import ScrollableTabsButtonForce from './ScrollableTabs'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <VerticalTabs /> 
+       <ScrollableTabsButtonForce /> 
       </header>
     </div>
   );
---
+++ src/ScrollableTabs.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import BackspaceIcon from '@material-ui/icons/Backspace';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`scrollable-force-tabpanel-${index}`}
+            aria-labelledby={`scrollable-force-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `scrollable-force-tab-${index}`,
+        'aria-controls': `scrollable-force-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function ScrollableTabsButtonForce() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" color="default">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    variant="scrollable"
+                    scrollButtons="on"
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="scrollable force tabs example"
+                >
+                    <Tab label="Upload Image" icon={<CloudUploadIcon />} {...a11yProps(0)} />
+                    <Tab label="Search Images" icon={<SearchIcon />} {...a11yProps(1)} />
+                    <Tab label="Related Images" icon={<AccountTreeIcon />} {...a11yProps(2)} />
+                    <Tab label="Remove Tags" icon={<BackspaceIcon />} {...a11yProps(3)} />
+                    <Tab label="Delete Image" icon={<DeleteForeverIcon />} {...a11yProps(4)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                Select an image to upload:
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                Search Image:
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                related Images:
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Select tags to be removed:
+            </TabPanel>
+            <TabPanel value={value} index={4}>
+                Select image to be delted:
+            </TabPanel>
+        </div>
+    );
+}
