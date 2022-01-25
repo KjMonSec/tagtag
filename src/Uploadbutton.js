2db36878bf667d244af1ee166d33f46372620ccf
pxmayang <peyangpetrina18@gmail.com>
2022-01-26 02:11:56 +0400

    Modify button

--- src/Uploadbutton.js
+++ src/Uploadbutton.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 
 const useStyles = makeStyles((theme) => ({
@@ -28,15 +27,9 @@ export default function UploadButton() {
         type="file"
       />
       <label htmlFor="contained-button-file">
-        <Button variant="contained" color="primary" component="span">
+        <Button variant="contained" fullWidth color="primary" component="span" startIcon = {<PhotoCamera />}>
           Upload
         </Button>
-      </label>
-      <input accept="image/*" className={classes.input} id="icon-button-file" type="file" />
-      <label htmlFor="icon-button-file">
-        <IconButton color="primary" aria-label="upload picture" component="span">
-          <PhotoCamera />
-        </IconButton>
       </label>
     </div>
   );
