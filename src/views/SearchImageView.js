95ed692f47de70d8de2db29bf7b32a68761d2b48
pxmayang <peyangpetrina18@gmail.com>
2022-01-29 23:20:28 +0400

    Fix image search integration

--- src/views/SearchImageView.js
+++ src/views/SearchImageView.js
@@ -6,13 +6,6 @@ import MultipleValueTextInput from 'react-multivalue-text-input';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
 
-const images = [
-    'https://preview.redd.it/acwq7czy2vz71.png?width=640&crop=smart&auto=webp&s=6f05fc3fd517e6bc79d9a963e13a3cb4b61a2099',
-    'https://static2.cbrimages.com/wordpress/wp-content/uploads/2021/12/Bojji-holding-a-stick-in-Ranking-of-Kings.jpg?q=50&fit=crop&w=960&h=500&dpr=1.5',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwjd2L4nHvq3SmktlDwCH4gSc7om9NhdmB8g&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmpfMGCfU0frSa4xsp31m5irELqnlZ_U-5Jg&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYHd4nhm7Ji3L0DMrHi3dYJ9_x0X_3zM4qkQ&usqp=CAU'
-]
 
 const useStyles = makeStyles((theme) => ({
     form: {
@@ -27,8 +20,6 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
-
 export default function SearchImageView(props) {
     const classes = useStyles();
     const [imageList, setImageList] = useState([])
@@ -36,8 +27,10 @@ export default function SearchImageView(props) {
     async function handleSubmit() {
         // get the tags from the ui and save them to a variable
         var tags = document.getElementById("search-tags").value.split(',');
+
         // send the request to the server with the tags and get the response
-        var res = await fetch('https://13odoj44e4.execute-api.us-east-1.amazonaws.com/v1/tags', {
+        var api = 'https://13odoj44e4.execute-api.us-east-1.amazonaws.com/v1/tags';
+        var res = await fetch(api, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -50,7 +43,9 @@ export default function SearchImageView(props) {
         res = await res.json();
 
         // render the images listed in the response
-        setImageList(images.urls);
+        if (Array.isArray(res.urls)) {
+            setImageList(res.urls);
+        }
     }
 
     return (
@@ -73,7 +68,7 @@ export default function SearchImageView(props) {
                 <ImageList rowHeight={180} className={classes.imageList}>
                     {imageList.map(image => (
                         <ImageListItem key={image}>
-                            <img src={image} alt={image}/>
+                            <img src={image} alt={image} />
                         </ImageListItem>
                     ))}
                 </ImageList>
