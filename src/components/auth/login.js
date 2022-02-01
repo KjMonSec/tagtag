b32c2b0447d5bbc49dce74a2f1a332628335fd41
pxmayang <peyangpetrina18@gmail.com>
2022-02-01 14:29:32 +0400

    add new login and signup page

---
+++ src/components/auth/login.js
@@ -0,0 +1,12 @@
+
+/**
+ * A function that connects with aws cognito to loggin and returns 
+ * a promise that resolves into a session access token
+ * @param {string} email 
+ * @param {string} password 
+ * @returns {string | false} The login session's access token on 
+ * success and false on failure
+ */
+export default async function login(email, password) {
+    // Add your code here
+}
